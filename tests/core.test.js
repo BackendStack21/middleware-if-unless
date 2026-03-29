bc4ca8936786220e431ac6ed397b7bd2ffc73a1d
Rolando Santamaria Maso <kyberneees@gmail.com>
2026-03-29 16:27:33 +0200

    fix: resolve JavaScript Standard Style linting violations
    - Fix 27 Chai assertion violations in tests/docs-examples.test.js
      Replace .to.be.undefined with .to.equal(undefined) for proper assertion syntax
    - Reorganize test structure: move core tests to tests/core.test.js
    - Update documentation with comprehensive examples and API reference
    - Update package.json with test configuration

--- tests/core.test.js
+++ tests/core.test.js
@@ -1,7 +1,7 @@
 /* global describe, it */
 const expect = require('chai').expect
 const request = require('supertest')
-const iffUnless = require('./index')()
+const iffUnless = require('../index')()
 
 const middleware = function (req, res, next) {
   res.body = 'hit'
